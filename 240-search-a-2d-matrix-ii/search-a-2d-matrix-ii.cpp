6073decef9d898c2c6ea2f3e6432bf685b09f167
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-04-17 07:37:33 +0530

    Time: 27 ms (99.52%) | Memory: 16.6 MB (98.27%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.cpp
@@ -0,0 +1,20 @@
+static auto _ = []() {
+    ios::sync_with_stdio(false); 
+    cin.tie(nullptr); 
+
+    return 0; 
+} ();
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size(),m=matrix[0].size();
+        int row=0;
+        int col=m-1;
+        while(row<n && col>=0){
+            if(matrix[row][col]==target)return true;
+            else if(matrix[row][col]>target)col--;
+            else row++;
+        }
+        return false;      
+    }
+};
