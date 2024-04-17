221040cd8e5fc457953ea5e77afce90188d13390
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-04-17 17:37:59 +0530

    Time: 4 ms (21.39%) | Memory: 8.4 MB (35.57%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    void generate(int i, int k, int left, int sum, int target, vector<int>& sub,
+    void generate(int i, int k, int sum, int target, vector<int>& sub,
                   vector<vector<int>>& ans) {
         if(target<k)return;
         if (sub.size()==k){
@@ -11,16 +11,16 @@ public:
         
         sub.push_back(i);
         sum += i;
-        generate(i+1, k, target - i, sum, target, sub, ans);
+        generate(i+1, k, sum, target, sub, ans);
         sub.pop_back();
         sum -= i;
-        generate(i + 1, k, target - i, sum, target, sub, ans);
+        generate(i + 1, k, sum, target, sub, ans);
     }
 
     vector<vector<int>> combinationSum3(int k, int n) {
         vector<int>sub;
         vector<vector<int>>ans;
-        generate(1,k,-1,0,n,sub,ans);    
+        generate(1,k,0,n,sub,ans);    
         return ans;
     }
 };
