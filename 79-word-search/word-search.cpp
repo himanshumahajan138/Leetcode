78332e06d51d87552da322f642867fa59f7820ca
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-04-18 15:47:11 +0530

    Time: 1010 ms (19.90%) | Memory: 11.8 MB (13.39%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,40 +1,37 @@
 class Solution {
 public:
-    bool isExist = false;
-    void backtrack(string &word, string &solution, int row, int col, int const rowSize, int const colSize, vector<vector<char>> &board,vector<vector<int>> &visited){
-        if(solution.back() != word.at(solution.size()-1) || visited.at(row).at(col) > 0){ //reject
-            return;
-        }
-        if(solution == word){
-            isExist = true;
-            return;
-        }
-        visited.at(row).at(col)++;
-        vector<int> DIR = {0, 1, 0, -1, 0};
-        for(int i = 0; i < 4; i++){
-            int new_row = row + DIR[i];
-            int new_col = col + DIR[i+1];
-            if(new_row < 0 || new_row > rowSize-1 || new_col < 0 || new_col > colSize-1) continue;
-            solution.push_back(board.at(new_row).at(new_col));
-            backtrack(word, solution, new_row, new_col, rowSize, colSize, board, visited);
-            solution.pop_back();
-            if(isExist) return;
-        }
+    bool find_next(int row, int col, int i, int m, int n,
+                   vector<vector<char>>& board, string word) {
+
+        if (i == word.length())
+            return true;
+
+        if (row < 0 || col < 0 || row == m || col == n ||
+            word[i] != board[row][col] || board[row][col] == '!')
+            return false;
+
+        char current = board[row][col];
+        board[row][col] = '!';
+
+        bool top = find_next(row - 1, col, i + 1, m, n, board, word);
+        bool bottom = find_next(row + 1, col, i + 1, m, n, board, word);
+        bool right = find_next(row, col + 1, i + 1, m, n, board, word);
+        bool left = find_next(row, col - 1, i + 1, m, n, board, word);
+
+        board[row][col]=current;
+
+        return top || bottom || right || left;
     }
+
     bool exist(vector<vector<char>>& board, string word) {
-        if(word == "ABCEFSADEESE" && board.size() == 3 && board[0].size() != 1) return true;
-        if(word == "ABCDEB" && board.size() == 2 && board[0].size() == 3) return true;
-        if(word == "AAaaAAaAaaAaAaA" && board.size() == 3) return true;
-        int const rowSize = board.size();
-        int const colSize = board[0].size();
-        for(int row = 0; row < rowSize; ++row){
-            for(int col = 0; col < colSize; ++col){
-                if(board[row][col] != word[0]) continue;
-                string solution = "";
-                vector<vector<int>> visited(rowSize, vector<int>(colSize, 0));
-                solution.push_back(board[row][col]);
-                backtrack(word, solution, row, col, rowSize, colSize, board, visited);
-                if(isExist) return isExist;
+        int m = board.size();
+        int n = board[0].size();
+        for (int row = 0; row < m; row++) {
+            for (int col = 0; col < n; col++) {
+                if (board[row][col] == word[0]) {
+                    if (find_next(row, col, 0, m, n, board, word))
+                        return true;
+                }
             }
         }
         return false;
