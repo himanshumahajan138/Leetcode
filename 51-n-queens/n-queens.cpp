f761aab8bf7849ba028449bce274a1daaf111f08
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-04-19 13:28:31 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (66.30%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,3 +1,9 @@
+const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
 class Solution {
 public:
     void place(int col, vector<int>& lr, vector<int>& uld, vector<int>& lld,
