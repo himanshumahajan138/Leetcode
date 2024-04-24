5992b30205099a9230f8de987ad37aaa2a4b8297
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-04-24 07:44:22 +0530

    Time: 2 ms (50.95%) | Memory: 7 MB (81.41%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
     int tribonacci(int n) {
-        int mem[n+3];
-        mem[0]=0;
-        mem[1]=1;
-        mem[2]=1;
+        int dp[] ={0,1,1};
         for(int i=3;i<=n;i++){
-            mem[i]=mem[i-3]+mem[i-2]+mem[i-1];
+            dp[i%3]=dp[0]+dp[1]+dp[2];
         }
-        return mem[n];
+        return dp[n%3];
     }
 };
