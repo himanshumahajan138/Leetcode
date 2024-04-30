cfd823f66b6d81a4c64c9691970a17cf7109bf24
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-04-30 23:20:39 +0530

    Time: 30 ms (99.40%) | Memory: 16.6 MB (59.88%) - LeetSync

---
+++ 2044-number-of-wonderful-substrings/number-of-wonderful-substrings.cpp
@@ -0,0 +1,20 @@
+const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+public:
+    long long wonderfulSubstrings(string word) {
+        long cnt[1024] = {1}, mask = 0, res = 0;
+        for (auto ch : word) {
+            mask ^= 1 << (ch - 'a');
+            res += cnt[mask];
+            for (auto n = 0; n < 10; ++n)
+                res += cnt[mask ^ (1 << n)];
+            ++cnt[mask];
+        }
+        return res;
+    }
+};
