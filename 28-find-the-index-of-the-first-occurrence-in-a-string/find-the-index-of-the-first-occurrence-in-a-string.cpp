c525cec27d7200055d499115cd0932d7a157ef53
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-03 09:19:47 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (78.20%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,10 +1,22 @@
+const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int m=haystack.length(),n=needle.length();
-        if(m<n)return -1;
-        for(int i=0 ; i<=(m-n) ; ++i){
-            if(haystack.substr(i,n)==needle)return i;
+        int m = haystack.length(), n = needle.length();
+        if (m < n)
+            return -1;
+        for (int i = 0; i <= (m - n); ++i) {
+            int j = 0;
+            while (j < n && haystack[i + j] == needle[j]) {
+                ++j;
+            }
+            if (j == n)
+                return i;
         }
         return -1;
     }
