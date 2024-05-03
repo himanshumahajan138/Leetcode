4fc12135483a1809fa340646fbb8ff25e6502522
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-03 14:15:16 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (57.28%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,18 @@
+const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0, twos = 0;
+        for (auto x : nums) {
+            ones = (ones ^ x) & (~twos); // add to ones if not present in twos
+            twos = (twos ^ x) & (~ones); // add to twos if present in ones, then
+                                         // next time it gets deleted from ones
+        }
+        return ones;
+    }
+};
