fee67cb8c5f4140cdc6908532767a8220f26fd08
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-05 19:46:15 +0530

    Time: 62 ms (25.00%) | Memory: 28.5 MB (12.50%) - LeetSync

--- 3384-minimum-number-of-operations-to-make-word-k-periodic/minimum-number-of-operations-to-make-word-k-periodic.cpp
+++ 3384-minimum-number-of-operations-to-make-word-k-periodic/minimum-number-of-operations-to-make-word-k-periodic.cpp
@@ -1,18 +1,18 @@
+const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
 class Solution {
 public:
     int minimumOperationsToMakeKPeriodic(string word, int k) {
-        unordered_map<string, int>count;
-        
-        for (int i = 0; i < word.length(); i += k) {
-            string seg = word.substr(i, k);
-            ++count[seg];
+        const int n = word.length();
+        int m = 0;
+        unordered_map<string, int> have;
+        for (int i = 0; i < n; i += k) {
+            m = max(m, ++have[word.substr(i, k)]);
         }
-        
-        int maxcount = 0;
-        for (auto& it : count) {
-            maxcount = max(maxcount, it.second);
-        }
-        
-        return (word.length()/k)-maxcount;
+        return n / k - m;
     }
 };
