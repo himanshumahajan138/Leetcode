77efa346f01395289e3ced7504e44a1934be572f
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-05 19:17:31 +0530

    Time: 10 ms (6.56%) | Memory: 11.3 MB (43.54%) - LeetSync

--- 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
+++ 237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.cpp
@@ -6,24 +6,9 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- const static auto initialize = [] {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return nullptr;
-}();
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-        /*
-        Intitutive solution 
-
-        ListNode *nextNode = node->next;
-        node->val = nextNode->val;
-        node->next = nextNode->next;
-        nextNode->next = nullptr;
-        delete(nextNode);
-        */
         node->val=node->next->val;
         node->next=node->next->next;
     }
