71b17befe0401c543d0bc326da97cf5368899720
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-07 10:17:37 +0530

    Time: 183 ms (45.06%) | Memory: 121.1 MB (50.70%) - LeetSync

---
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+
+    int calculate(ListNode* head , int carry){
+        if(head->next!=NULL){
+            carry=calculate(head->next,carry);
+        }
+        int mul= head->val * 2 + carry;
+        if(mul>9){
+            carry=mul/10;
+            mul=mul%10;
+        }
+        else carry=0;
+        
+        head->val=mul;
+        return carry;
+    }
+
+    ListNode* doubleIt(ListNode* head) {
+        int carry=calculate(head,0);
+        if(carry>0){
+            ListNode* new_head = new ListNode(carry,head);
+            return new_head;
+        }
+        return head;
+    }
+};
