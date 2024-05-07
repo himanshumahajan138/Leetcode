2e5d99de0a17374c56e98997f203f52623822de3
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-07 10:19:35 +0530

    Time: 172 ms (66.48%) | Memory: 121.3 MB (41.17%) - LeetSync

--- 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
+++ 2871-double-a-number-represented-as-a-linked-list/double-a-number-represented-as-a-linked-list.cpp
@@ -8,29 +8,34 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+ const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
 class Solution {
 public:
+    int calculate(ListNode* head, int carry) {
+        if (head->next != NULL) {
+            carry = calculate(head->next, carry);
+        }
+        int mul = head->val * 2 + carry;
+        if (mul > 9) {
+            carry = mul / 10;
+            mul = mul % 10;
+        } else
+            carry = 0;
 
-    int calculate(ListNode* head , int carry){
-        if(head->next!=NULL){
-            carry=calculate(head->next,carry);
-        }
-        int mul= head->val * 2 + carry;
-        if(mul>9){
-            carry=mul/10;
-            mul=mul%10;
-        }
-        else carry=0;
-        
-        head->val=mul;
+        head->val = mul;
+
         return carry;
     }
 
     ListNode* doubleIt(ListNode* head) {
-        int carry=calculate(head,0);
-        if(carry>0){
-            ListNode* new_head = new ListNode(carry,head);
-            return new_head;
+        int carry = calculate(head, 0);
+        if (carry > 0) {
+            return new ListNode(carry, head);
         }
         return head;
     }
