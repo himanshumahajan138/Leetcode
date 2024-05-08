04d09db8ca71f2bcf88e298634186ffc50fc7d74
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-08 08:53:06 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (6.54%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -7,18 +7,18 @@ const static auto initialize = [] {
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>ans(nums1.size());
-        stack<int>st;
-        unordered_map<int,int>mpp;
-        for(int i=nums2.size()-1 ; i>=0 ; --i){
-            while(!st.empty() && st.top()<=nums2[i]){
+        vector<int> ans(nums1.size());
+        stack<int> st;
+        unordered_map<int, int> mpp;
+        for (int i = nums2.size() - 1; i >= 0; --i) {
+            while (!st.empty() && st.top() <= nums2[i]) {
                 st.pop();
             }
             mpp[nums2[i]] = st.empty() ? -1 : st.top();
             st.push(nums2[i]);
         }
-        for(int i=0 ; i<nums1.size() ; ++i){
-            ans[i]=mpp[nums1[i]];
+        for (int i = 0; i < nums1.size(); ++i) {
+            ans[i] = mpp[nums1[i]];
         }
         return ans;
     }
