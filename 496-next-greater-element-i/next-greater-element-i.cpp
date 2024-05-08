989069add637188d2e2fdf7d94c39650bffb32b8
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-08 08:51:47 +0530

    Time: 7 ms (38.78%) | Memory: 12.2 MB (26.87%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.cpp
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -14,11 +14,7 @@ public:
             while(!st.empty() && st.top()<=nums2[i]){
                 st.pop();
             }
-
-            if(st.empty())mpp[nums2[i]]=-1;
-            else {
-                mpp[nums2[i]]=st.top();
-            }
+            mpp[nums2[i]] = st.empty() ? -1 : st.top();
             st.push(nums2[i]);
         }
         for(int i=0 ; i<nums1.size() ; ++i){
