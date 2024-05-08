cad56898e67a16868340c3ed22279ba2a206a596
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-08 07:46:50 +0530

    Time: 14 ms (29.82%) | Memory: 14.4 MB (55.66%) - LeetSync

--- 506-relative-ranks/relative-ranks.cpp
+++ 506-relative-ranks/relative-ranks.cpp
@@ -7,22 +7,27 @@ const static auto initialize = [] {
 class Solution {
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
-        unordered_map<int,int>mpp;
-        for(int i=0 ; i<score.size() ; ++i){
-            mpp[score[i]]=i;
+
+        unordered_map<int, int> mpp;
+
+        for (int i = 0; i < score.size(); ++i) {
+            mpp[score[i]] = i;
         }
-        sort(score.begin(),score.end(),greater<int>());
-        vector<string>ranks;
-        for(int i=0 ; i<score.size() ; ++i){
-            if(i==0)ranks.push_back("Gold Medal");
-            else if(i==1)ranks.push_back("Silver Medal");
-            else if(i==2)ranks.push_back("Bronze Medal");
-            else ranks.push_back(to_string(i+1));
+
+        sort(score.begin(), score.end(), greater<int>());
+
+        vector<string> ranks(score.size());
+
+        for (int i = 0; i < score.size(); ++i) {
+            if (i == 0)
+                ranks[mpp[score[i]]] = "Gold Medal";
+            else if (i == 1)
+                ranks[mpp[score[i]]] = "Silver Medal";
+            else if (i == 2)
+                ranks[mpp[score[i]]] = "Bronze Medal";
+            else
+                ranks[mpp[score[i]]] = to_string(i + 1);
         }
-        vector<string>ans(score.size());
-        for(int i=0 ; i<score.size() ; ++i){
-            ans[mpp[score[i]]]=ranks[i];
-        }
-        return ans;
+        return ranks;
     }
 };
