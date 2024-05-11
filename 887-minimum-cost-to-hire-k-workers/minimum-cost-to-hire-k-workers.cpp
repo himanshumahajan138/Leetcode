6e352f55dd84c0e73cdc7a7bc2d40d5082f0b935
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-11 08:26:08 +0530

    Time: 11 ms (100.00%) | Memory: 26.6 MB (38.83%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.cpp
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.cpp
@@ -5,19 +5,22 @@ const static auto initialize = [] {
     return nullptr;
 }();
 class Solution {
-public:
-        double mincostToHireWorkers(vector<int> q, vector<int> w, int K) {
-        vector<vector<double>> workers;
-        for (int i = 0; i < q.size(); ++i)
-            workers.push_back({(double)(w[i]) / q[i], (double)q[i]});
-        sort(workers.begin(), workers.end());
-        double res = DBL_MAX, qsum = 0;
-        priority_queue<int> pq;
-        for (auto worker: workers) {
-            qsum += worker[1], pq.push(worker[1]);
-            if (pq.size() > K) qsum -= pq.top(), pq.pop();
-            if (pq.size() == K) res = min(res, qsum * worker[0]);
-        }
-        return res;
+ public:
+  double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
+    double ans = DBL_MAX;
+    int sum = 0;
+    vector<pair<double, int>> w;
+    priority_queue<int> s;
+    for (int i = 0; i < quality.size(); ++i)
+      w.emplace_back((double)wage[i] / quality[i], quality[i]);
+    ranges::sort(w);
+    for (const auto& [wPerQ, q] : w) {
+      s.push(q);
+      sum += q;
+      if (s.size() > k)
+        sum -= s.top(), s.pop();
+      if (s.size() == k)
+        ans = min(ans, sum * wPerQ);
     }
+    return ans;}
 };
