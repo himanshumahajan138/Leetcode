61451f42f656e1467b93422ce4a6f7576fb69146
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-13 19:37:26 +0530

    Time: 2 ms (54.61%) | Memory: 10.5 MB (9.21%) - LeetSync

---
+++ 891-score-after-flipping-matrix/score-after-flipping-matrix.cpp
@@ -0,0 +1,19 @@
+const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+public:
+        int matrixScore(vector<vector<int>> A) {
+        int M = A.size(), N = A[0].size(), res = (1 << (N - 1)) * M;
+        for (int j = 1; j < N; j++) {
+            int cur = 0;
+            for (int i = 0; i < M; i++) cur += A[i][j] == A[i][0];
+            res += max(cur, M - cur) * (1 << (N - j - 1));
+        }
+        return res;
+    }
+};
