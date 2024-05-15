aacc7eb2361119f34ef7423f843e3652c8cc35f0
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-15 16:05:40 +0530

    Time: 7 ms (89.34%) | Memory: 10.7 MB (76.78%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -7,19 +7,17 @@ const static auto initialize = [] {
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char,int>mpp;
-        for (char ch : s){
-            ++mpp[ch];
+        unordered_map<char,int> freq;           //for frequency of characters
+        priority_queue<pair<int,char>> maxheap; //maxheap according to frequency of characters
+        for(char c: s)
+            freq[c]++;
+        for(auto it: freq)
+            maxheap.push({it.second,it.first}); //heap will be constructed on the basis of frequency
+        s="";   
+        while(!maxheap.empty()){
+            s+=string(maxheap.top().first,maxheap.top().second); //frequency times the character
+            maxheap.pop();
         }
-        map<int,string,greater<int>>hash;
-        for(auto it : mpp){
-            hash[it.second]+=string(it.second,it.first);
-        }
-        
-        string ans="";
-        for(auto it : hash){
-            ans+=it.second;
-        }
-        return ans;
+        return s;
     }
 };
