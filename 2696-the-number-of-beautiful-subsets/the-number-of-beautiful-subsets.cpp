6eccac9d55a22f2c53eeccc75cf43c30e2971c53
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-05-23 10:04:49 +0530

    Time: 10 ms (93.18%) | Memory: 37.5 MB (71.90%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,28 @@
+const static auto initialize = [] {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
+    return nullptr;
+}();
+class Solution {
+public:
+    int beautifulSubsets(vector<int>& nums, int k) {
+        int result = 1;
+        map<int, map<int, int>> freq;
+        for (int& x: nums) {
+            freq[x % k][x]++;
+        }
+        for (auto& fr: freq) {
+            int prevNum = -k, prev2, prev1 = 1, curr;
+            for (auto& [num, f]: fr.second) {
+                int skip = prev1;
+                int take = ((1 << f) - 1) * (num - prevNum == k ? prev2 : prev1);
+                curr = skip + take;
+                prev2 = prev1; prev1 = curr;
+                prevNum = num;
+            }
+            result *= curr;
+        }
+        return result - 1;
+    }
+};
