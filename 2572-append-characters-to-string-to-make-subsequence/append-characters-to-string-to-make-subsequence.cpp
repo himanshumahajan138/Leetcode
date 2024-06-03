0889c16486b330fe3ad1759323ae631acdbfe4c0
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-06-03 22:49:58 +0530

    Time: 4 ms (99.83%) | Memory: 11.9 MB (83.42%) - LeetSync

---
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -0,0 +1,23 @@
+const static auto initialize = [] {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
+
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int s_index = 0, t_index = 0;
+        int s_length = s.length(), t_length = t.length();
+    
+        while (s_index < s_length && t_index < t_length) {
+            if (s[s_index] == t[t_index]) {
+                t_index++;
+            }
+            s_index++;
+        }
+    
+        return t_length - t_index;
+    }
+};
