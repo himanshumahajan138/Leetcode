10b554dcaed5cea5de52699496db573daf06ccde
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-06-04 12:12:59 +0530

    Time: 5 ms (29.78%) | Memory: 9 MB (48.94%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char,int>mpp;
+        for(auto ch : s)++mpp[ch];
+        int len=0;
+        int max_odd_len=1;
+        int one_len_odd=0;
+        for(auto it : mpp){
+            if((it.second & 1)==0){
+                len+=it.second;
+            }
+            else {
+                if(it.second==1)++one_len_odd;
+                else if(it.second>max_odd_len){
+                    len+=(max_odd_len/2)*2;
+                    max_odd_len=it.second;
+                }
+                else len+=(it.second/2)*2;
+            }
+        }
+        if(max_odd_len>1)len+=max_odd_len;
+        else if(one_len_odd>0)++len;
+        return len;
+    }
+};
