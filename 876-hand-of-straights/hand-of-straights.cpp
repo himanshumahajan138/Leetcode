94c4f7f45de1fff25c075c0dd7b6f9fca1b12a82
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-06-06 10:32:04 +0530

    Time: 53 ms (75.70%) | Memory: 30.6 MB (69.25%) - LeetSync

---
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
+        if ((hand.size() % groupSize) == 0) {
+            map<int, int> freq;
+            for (auto x : hand)
+                ++freq[x];
+            for (auto i = freq.begin(); i != freq.end();) {
+                if (i->second > 0) {
+                    --freq[i->first];
+                    int j = i->first + 1;
+                    int k = groupSize;
+                    while (--k) {
+                        if (freq[j] < 1)
+                            return false;
+                        else {
+                            --freq[j];
+                            ++j;
+                        }
+                    }
+                }
+                if (i->second == 0)
+                    ++i;
+            }
+            return true;
+        } else
+            return false;
+    }
+};
