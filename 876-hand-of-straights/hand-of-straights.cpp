42b49af630f64677f5434b733902aa859bd91f84
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-06-06 10:32:32 +0530

    Time: 60 ms (57.08%) | Memory: 30.7 MB (42.24%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,3 +1,9 @@
+const static auto initialize = [] {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
