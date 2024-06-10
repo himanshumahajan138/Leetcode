bfb62392cd95f9085e21a643facbefdf8796b182
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-06-10 10:03:49 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (10.73%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,9 +1,4 @@
-const static auto initialize = [] {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
+
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
