1ac1724b1bc99cd4215a03d287c46bfc80dade7f
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-06-10 10:06:44 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (10.73%) - LeetSync

--- 1137-height-checker/height-checker.cpp
+++ 1137-height-checker/height-checker.cpp
@@ -1,13 +1,43 @@
-
 class Solution {
+    void countingSort(vector<int>& arr) {
+        // Create the counting hash map.
+        unordered_map<int, int> counts;
+        // Find the minimum and maximum values in the array.
+        int minVal = *min_element(arr.begin(), arr.end());
+        int maxVal = *max_element(arr.begin(), arr.end());
+
+        // Update element's count in the hash map.
+        for (auto& val : arr) {
+            counts[val]++;
+        }
+
+        int index = 0;
+        // Place each element in its correct position in the array.
+        for (int val = minVal; val <= maxVal; ++val) {
+            // Append all 'val's together if they exist.
+            while (counts[val] > 0) {
+                arr[index] = val;
+                index += 1;
+                counts[val] -= 1;
+            }
+        }
+    }
+
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int>temp=heights;
-        sort(temp.begin(),temp.end());
-        int cnt = 0;
-        for(int i=0 ; i<temp.size() ; ++i){
-            if(temp[i]!=heights[i])++cnt;
+        // Sort the array using counting sort.
+        vector<int> sortedHeights = heights;
+        countingSort(sortedHeights);
+
+        int count = 0;
+        // Loop through the original and sorted arrays.
+        for (int i = 0; i < sortedHeights.size(); ++i) {
+            // Increment count if elements at the same index differ.
+            if (heights[i] != sortedHeights[i]) {
+                count += 1;
+            }
         }
-        return cnt;
+        // Return the total count of differing elements.
+        return count;
     }
 };
