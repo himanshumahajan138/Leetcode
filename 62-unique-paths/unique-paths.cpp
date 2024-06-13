ce8b029d81d660ef6a653a4f4f903289f350ab71
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-06-13 15:56:13 +0530

    Time: 4 ms (12.01%) | Memory: 7.7 MB (16.88%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -11,7 +11,20 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp);
+
+        for(int i=0 ;  i<m ; ++i){
+            for(int  j =0  ;  j<n ;  ++j){
+                if(i==0 &&  j==0 )dp[0][0]=1;
+                else{
+                    int right=0 , down=0;
+                    if(j>0)right= dp[i][j-1];
+                    if(i>0)down=dp[i-1][j];
+
+                    dp[i][j]=right+down;
+                }
+            }
+        }
+        return dp[m-1][n-1];
 
     }
 };
