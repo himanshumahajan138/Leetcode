9d5350d9e9031998042fe75326a155a7c9296cb3
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-06-29 15:14:29 +0530

    Time: 20 ms (67.98%) | Memory: 19.3 MB (41.98%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,3 +1,9 @@
+const static auto initialize = [] {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return nullptr;
+}();
 class Solution {
 public:
     void dfs(int start,vector<int>&visited,vector<int>adj[]){
