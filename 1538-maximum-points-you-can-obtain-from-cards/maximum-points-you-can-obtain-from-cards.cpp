6114224af7363afc36f2fff108a5ab67bd70c00a
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-07-16 20:10:53 +0530

    Time: 22 ms (99.11%) | Memory: 44.8 MB (43.62%) - LeetSync

--- 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
+++ 1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.cpp
@@ -7,24 +7,20 @@ const static auto initialize = [] {
 class Solution {
 public:
     int maxScore(vector<int>& arr, int k) {
-        int ans=0;
-        int n=arr.size();
-        if(k==n){
-            for(auto a :arr)ans+=a;
-            return ans;
+        int ans = 0;
+        int n = arr.size();
+        int lsum = 0, rsum = 0;
+        for (int i = 0; i < k; ++i)
+            lsum += arr[i];
+        ans = lsum;
+        int l = n - 1;
+        for (int i = k - 1; i >= 0; --i) {
+            lsum -= arr[i];
+            rsum += arr[l];
+            ans = max(ans, lsum + rsum);
+            --l;
         }
-        else{
-            int lsum =0,rsum=0; 
-            for(int i=0 ; i<k ; ++i)lsum+=arr[i];
-            ans=lsum;
-            int l=n-1;
-            for(int i=k-1;i>=0;--i){
-                lsum-=arr[i];
-                rsum+=arr[l];
-                ans=max(ans,lsum+rsum);
-                --l;
-            }
-        }
+
         return ans;
     }
 };
