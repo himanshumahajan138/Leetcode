5940b2c85b93dcca06084e846c4a96fc8ff70fde
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-08-16 10:36:15 +0530

    Time: 133 ms (85.85%) | Memory: 132.1 MB (5.24%) - LeetSync

--- 204-count-primes/count-primes.cpp
+++ 204-count-primes/count-primes.cpp
@@ -1,24 +1,21 @@
-const static auto initialize = [] {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return nullptr;
-}();
 class Solution {
 public:
-    int countPrimes(int n) {
-        vector<int>prime(n+1,1);
-        for(int i=2;i*i<=n;++i){
-            if(prime[i]){
-                for(int j=i*i;j<=n;j+=i){
-                    prime[j]=0;
+    void seive_of_eratosthenes(vector<int>&seive, int n){
+        for(int i=2 ; i*i<n;++i){
+            if(seive[i]==1){
+                for(int j=i*i ; j<n ; j+=i){
+                    seive[j]=0;
                 }
             }
         }
-        int count=0;
-        for(int i = 2 ; i<n ;++i){
-            if(prime[i]==1)count++;
+    }
+    int countPrimes(int n) {
+        vector<int>seive(n,1);
+        seive_of_eratosthenes(seive,n);
+        int cnt=0;
+        for(int ele=2;ele<seive.size();++ele){
+            if(seive[ele]==1)++cnt;
         }
-        return count;
+        return cnt;
     }
 };
