3d667c7f45842b8f8b53644d54b19c698b78b7b8
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-10-10 23:00:33 +0530

    Time: 69 ms (16.29%) | Memory: 47 MB (23.94%) - LeetSync

--- 1002-maximum-width-ramp/maximum-width-ramp.cpp
+++ 1002-maximum-width-ramp/maximum-width-ramp.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     int maxWidthRamp(vector<int>& nums) {
-        stack<int> st;
+        int n = nums.size();
+        stack<int> s;
         
-        // First pass: build a stack of indices where each corresponding value in nums is in decreasing order
-        for (int i = 0; i < nums.size(); ++i) {
-            if (st.empty() || nums[i] < nums[st.top()]) {
-                st.push(i);
+        // Step 1: Build a decreasing stack of indices
+        for (int i = 0; i < n; ++i) {
+            if (s.empty() || nums[s.top()] > nums[i]) {
+                s.push(i);
             }
         }
         
-        int maxo = 0;
+        int maxWidth = 0;
         
-        // Second pass: traverse from the end to the start to find the maximum width ramp
-        for (int i = nums.size() - 1; i >= 0; --i) {
-            while (!st.empty() && nums[i] >= nums[st.top()]) {
-                maxo = max(maxo, i - st.top());
-                st.pop();
+        // Step 2: Traverse from the end and find maximum width ramp
+        for (int j = n - 1; j >= 0; --j) {
+            while (!s.empty() && nums[s.top()] <= nums[j]) {
+                maxWidth = max(maxWidth, j - s.top());
+                s.pop();
             }
         }
         
-        return maxo;
+        return maxWidth;
     }
 };
