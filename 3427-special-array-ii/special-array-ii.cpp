46443a5a3f2c0cc9eb7f1e89153a695f72790382
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2024-12-09 23:07:01 +0530

    Time: 6 ms (75.00%) | Memory: 126.6 MB (66.56%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -1,55 +1,26 @@
-const static auto initialize = [] {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return nullptr;
-}();
-
 class Solution {
 public:
-    vector<bool> special(vector<int>& nums, vector<vector<int>>& queries) {
-        
+    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
         int n = nums.size();
-        if (n < 2) {
-            vector<bool> ans(queries.size(), true);
-            return ans;
-        }
-
-        vector<int> pdiff(n - 1, 0);
-
+        vector<int> prefix(n, 0); // Initialize prefix array with size n
         
-        for (int i = 0; i < n - 1; ++i) {
-            pdiff[i] = ((nums[i] & 1) != (nums[i + 1] & 1)) ? 1 : 0;
-        }
-
-
-        vector<int> ppdiff(n, 0);
-        for (int i = 0; i < n - 1; ++i) {
-            ppdiff[i + 1] = ppdiff[i] + pdiff[i];
-        }
-
-
-        vector<bool> ans;
-        for (auto& query : queries) {
-            
-            int from = query[0];
-            int to = query[1];
-            
-            if (to == from) {
-                ans.push_back(true); 
-            } 
-            else {
-                int expected = to - from;
-                int actual = ppdiff[to] - ppdiff[from];
-                
-                ans.push_back(actual == expected);
+        // Calculate the prefix array
+        for (int i = 1; i < n; i++) {
+            prefix[i] = prefix[i - 1];
+            if ((nums[i - 1] % 2 == 0 && nums[i] % 2 == 0) || (nums[i - 1] % 2 != 0 && nums[i] % 2 != 0)) {
+                prefix[i]++;
             }
         }
-        return ans;
-    }
-    
-    vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
-       
-        return special(nums,queries);
+
+        vector<bool> ans; // Result vector
+        
+        // Process the queries
+        for (auto& q : queries) {
+            int left = q[0], right = q[1];
+            int specialCount = prefix[right] - (left > 0 ? prefix[left] : 0);
+            ans.push_back(specialCount == 0);
+        }
+
+        return ans; // Return the result
     }
 };
