076ca92e95b451c241f3199ccf29edf2a3b1a6c6
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2025-05-11 23:37:20 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (59.60%) - LeetSync

--- 1293-three-consecutive-odds/three-consecutive-odds.cpp
+++ 1293-three-consecutive-odds/three-consecutive-odds.cpp
@@ -1,15 +1,9 @@
-const static auto initialize = [] {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
 class Solution {
 public:
     bool threeConsecutiveOdds(vector<int>& arr) {
-        if(arr.size()<3)return false;
-        for(int i=2 ; i<arr.size();++i){
-            if((arr[i]&1)==1 && (arr[i-1]&1)==1 && (arr[i-2]&1)==1)return true;
+        int n = arr.size();
+        for(int i=0;i<n-2;i++){
+            if(arr[i]&1 && arr[i+1]&1 && arr[i+2]&1) return true;
         }
         return false;
     }
