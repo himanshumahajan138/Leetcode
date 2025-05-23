532eae3b6acae4704a15da07b1cdb5ca48b84dc8
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2025-05-23 22:52:36 +0530

    Time: 19 ms (32.26%) | Memory: 133.1 MB (27.74%) - LeetSync

--- 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
+++ 3307-find-the-maximum-sum-of-node-values/find-the-maximum-sum-of-node-values.cpp
@@ -1,19 +1,20 @@
-const static auto initialize = [] {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return nullptr;
-}();
-
 class Solution {
 public:
     long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-    long long sum = 0, cnt = 0, sacrifice = INT_MAX;
-    for (long long n : nums) {
-        sum += max(n ^ k, n);
-        cnt += (n ^ k) > n;
-        sacrifice = min(sacrifice, abs(n - (n ^ k)));
+        long long sum = 0;
+        vector<long long> res;
+        for (int x : nums) {
+            sum += x;
+            int y = x ^ k;
+            res.push_back(y - x);
+        }
+        sort(res.begin(), res.end());
+        reverse(res.begin(), res.end());
+        
+        for (int i = 0; i < res.size() - 1; i += 2) {
+            if (res[i] + res[i + 1] <= 0) break;
+            sum += res[i] + res[i + 1];
+        }
+        return sum;
     }
-    return sum - (cnt % 2 ? sacrifice : 0);
-}
 };
