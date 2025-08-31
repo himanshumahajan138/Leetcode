d0aad0ef21b421b8acb69d0c214b3ac4288cd332
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2025-08-31 23:20:13 +0530

    Time: 131 ms (91.61%) | Memory: 8.9 MB (27.06%) - LeetSync

--- 37-sudoku-solver/sudoku-solver.cpp
+++ 37-sudoku-solver/sudoku-solver.cpp
@@ -1,41 +1,54 @@
-const static auto initialize = [] {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-    return nullptr;
-}();
 class Solution {
-public:
+    int n = 3, N = 9;
+    int rows[9][10] = {}, cols[9][10] = {}, boxes[9][10] = {};
+    vector<vector<char>>* boardPtr;
+    bool sudokuSolved = false;
 
-    bool is_valid(int row,int col,char&c,vector<vector<char>>&board){
-        for(int i=0;i<9;i++){
-            if(board[row][i]==c)return false;//if same char found in row 
-            if(board[i][col]==c)return false;//if same char found in col
-            if(board[3*(row/3)+i/3][3*(col/3)+i%3]==c)return false;//found in 3x3 part
-        }//above formula is very very important to understand
-        return true;
+    bool couldPlace(int d, int row, int col) {
+        int idx = (row / n) * n + col / n;
+        return rows[row][d] + cols[col][d] + boxes[idx][d] == 0;
     }
 
-    bool solve(vector<vector<char>>&board){
-        int n = board.size();
-        for(int row=0 ; row<n ; row++){//for traversing in rows 
-            for(int col=0;col<n;col++){//for traversing in columns
-                if(board[row][col]=='.'){//if there is some empty place in board 
-                    for(char c='1';c<='9';c++){//check all values of char '1' to '9' 
-                        if(is_valid(row,col,c,board)){//if for any value its valid 
-                            board[row][col]=c;//take condition change the board value
-                            if(solve(board))return true;//again check for next blank
-                            else board[row][col]='.';//if unable to solve so backtrack
-                        }
-                    }
-                    return false;//not solved for any value of char '1' to '9'
+    void placeNumber(int d, int row, int col) {
+        int idx = (row / n) * n + col / n;
+        rows[row][d]++;
+        cols[col][d]++;
+        boxes[idx][d]++;
+        (*boardPtr)[row][col] = d + '0';
+    }
+
+    void removeNumber(int d, int row, int col) {
+        int idx = (row / n) * n + col / n;
+        rows[row][d]--;
+        cols[col][d]--;
+        boxes[idx][d]--;
+        (*boardPtr)[row][col] = '.';
+    }
+
+    void placeNextNumbers(int row, int col) {
+        if (row == N - 1 && col == N - 1) sudokuSolved = true;
+        else if (col == N - 1) backtrack(row + 1, 0);
+        else backtrack(row, col + 1);
+    }
+
+    void backtrack(int row, int col) {
+        if ((*boardPtr)[row][col] == '.') {
+            for (int d = 1; d <= 9; d++) {
+                if (couldPlace(d, row, col)) {
+                    placeNumber(d, row, col);
+                    placeNextNumbers(row, col);
+                    if (!sudokuSolved) removeNumber(d, row, col);
                 }
             }
-        }
-        return true;//if there is no blank left in board then means its solved so true
+        } else placeNextNumbers(row, col);
     }
 
+public:
     void solveSudoku(vector<vector<char>>& board) {
-        solve(board);
+        boardPtr = &board;
+        for (int i = 0; i < N; i++)
+            for (int j = 0; j < N; j++)
+                if (board[i][j] != '.') placeNumber(board[i][j] - '0', i, j);
+        backtrack(0, 0);
     }
 };
