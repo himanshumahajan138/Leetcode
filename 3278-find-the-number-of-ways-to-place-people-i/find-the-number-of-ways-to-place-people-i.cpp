7b5f547d19dfe78195b8bce33aacec898ea16300
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2025-09-03 00:20:18 +0530

    Time: 0 ms (100.00%) | Memory: 32.6 MB (39.26%) - LeetSync

---
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    static bool cmp(vector<int>& p, vector<int>& q){
+        return (p[0]==q[0])?p[1]<q[1]:p[0]>q[0];// order by (x, >)
+    }
+
+    int numberOfPairs(vector<vector<int>>& P) {
+        sort(P.begin(), P.end(), cmp);
+        int n = P.size(), ans = 0;
+        for(int i=0; i<n-1; i++){
+            int y=INT_MAX;
+            for(int j = i+1; j<n; j++){
+                if (P[j][1]>=P[i][1] && y>P[j][1]){
+                    ans++;
+                    y=P[j][1];
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
