07dad7f69aa9f50be09ac3398a8d8f6ef54dd3b3
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2025-11-22 01:05:13 +0530

    Time: 19 ms (97.73%) | Memory: 14.1 MB (92.50%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.cpp
@@ -1,29 +1,23 @@
 class Solution {
 public:
     int countPalindromicSubsequence(string s) {
-        vector<int> R(26,0);
-        for(auto &u : s)
-        {
-            R[u-'a']++;
+        int n = s.size();
+        vector<int> first(26, -1), last(26, -1);
+        for (int i = 0; i < n; i++) {
+            int c = s[i] - 'a';
+            if (first[c] == -1) first[c] = i;
+            last[c] = i;
         }
-        vector<int> L(26, 0);
-        
-        unordered_set<int> S;
-        
-        for(int i = 0; i < s.length(); i++)
-        {
-            int t = s[i] - 'a';
-            R[t]--;
-            for(int j = 0; j < 26; j++)
-            {
-                if(L[j] > 0 && R[j] > 0)
-                {
-                    S.insert(26*t + j);
+        int ans = 0;
+        for (int c = 0; c < 26; c++) {
+            if (first[c] != -1 && last[c] - first[c] > 1) {
+                int mask = 0;
+                for (int i = first[c] + 1; i < last[c]; i++) {
+                    mask |= 1 << (s[i] - 'a');
                 }
+                ans += __builtin_popcount(mask);
             }
-            L[t]++;
         }
-        
-        return S.size();
+        return ans;
     }
 };
