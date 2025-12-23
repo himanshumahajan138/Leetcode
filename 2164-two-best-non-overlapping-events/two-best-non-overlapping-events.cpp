8cf8832affa37d0bd824fde0a067d89bdd48b271
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2025-12-23 22:05:17 +0530

    Time: 78 ms (64.09%) | Memory: 158.4 MB (99.23%) - LeetSync

--- 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
+++ 2164-two-best-non-overlapping-events/two-best-non-overlapping-events.cpp
@@ -2,41 +2,37 @@ class Solution {
 public:
     int maxTwoEvents(vector<vector<int>>& events) {
         int n = events.size();
-        
-        sort(events.begin(), events.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[0] < b[0];
-        });
-        
-        vector<int> suffixMax(n);
-        suffixMax[n - 1] = events[n - 1][2];
-        
-        for (int i = n - 2; i >= 0; --i) {
-            suffixMax[i] = max(events[i][2], suffixMax[i + 1]);
+        sort(events.begin(), events.end());
+        vector<int> maxSuffix(n);
+        maxSuffix[n - 1] = events[n - 1][2];
+
+        for (int i = n - 2; i >= 0; i--) {
+            maxSuffix[i] = max(maxSuffix[i + 1], events[i][2]);
         }
-        
-        int maxSum = 0;
-        
-        for (int i = 0; i < n; ++i) {
-            int left = i + 1, right = n - 1;
-            int nextEventIndex = -1;
-            
-            while (left <= right) {
-                int mid = left + (right - left) / 2;
-                if (events[mid][0] > events[i][1]) {
-                    nextEventIndex = mid;
-                    right = mid - 1;
+
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int currValue = events[i][2];
+            ans = max(ans, currValue);
+
+            int nextStart = events[i][1] + 1;
+            int low = i + 1, high = n - 1, idx = -1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (events[mid][0] >= nextStart) {
+                    idx = mid;
+                    high = mid - 1;
                 } else {
-                    left = mid + 1;
+                    low = mid + 1;
                 }
             }
-            
-            if (nextEventIndex != -1) {
-                maxSum = max(maxSum, events[i][2] + suffixMax[nextEventIndex]);
+
+            if (idx != -1) {
+                ans = max(ans, currValue + maxSuffix[idx]);
             }
-            
-            maxSum = max(maxSum, events[i][2]);
         }
-        
-        return maxSum;
+
+        return ans;
     }
 };
