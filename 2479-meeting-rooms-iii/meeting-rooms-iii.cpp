4642c060c61f7afc994e140c8365d6360bc80c9c
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2025-12-28 00:43:05 +0530

    Time: 63 ms (89.01%) | Memory: 98.1 MB (70.22%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.cpp
@@ -1,41 +1,33 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     int mostBooked(int n, vector<vector<int>>& meetings) {
         sort(meetings.begin(), meetings.end());
 
         priority_queue<int, vector<int>, greater<int>> free;
-        for (int i = 0; i < n; ++i) free.push(i);
+        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<>> busy;
+        vector<int> cnt(n);
 
-        using T = pair<long long,int>;               // {end, room}
-        priority_queue<T, vector<T>, greater<T>> busy;
+        for (int i = 0; i < n; i++) free.push(i);
 
-        vector<int> cnt(n, 0);
-
-        for (auto& m : meetings) {
-            long long s = m[0], e = m[1];
+        for (auto &m : meetings) {
+            long long s = m[0], d = m[1] - m[0];
 
             while (!busy.empty() && busy.top().first <= s) {
                 free.push(busy.top().second);
                 busy.pop();
             }
 
-            int room;
-            long long newEnd;
             if (!free.empty()) {
-                room = free.top(); free.pop();
-                newEnd = e;
+                int r = free.top(); free.pop();
+                busy.push({m[1], r});
+                cnt[r]++;
             } else {
-                auto [endTime, r] = busy.top(); busy.pop();
-                room = r;
-                newEnd = endTime + (e - s);
+                auto [t, r] = busy.top(); busy.pop();
+                busy.push({t + d, r});
+                cnt[r]++;
             }
-            busy.emplace(newEnd, room);
-            ++cnt[room];
         }
 
-        return int(max_element(cnt.begin(), cnt.end()) - cnt.begin());
+        return max_element(cnt.begin(), cnt.end()) - cnt.begin();
     }
 };
