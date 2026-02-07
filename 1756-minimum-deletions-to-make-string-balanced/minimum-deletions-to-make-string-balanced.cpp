cd123ed03a164b1ba5b2259cea0f9345b3419fb4
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2026-02-08 00:53:25 +0530

    Time: 12 ms (88.17%) | Memory: 25.6 MB (72.34%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     int minimumDeletions(string s) {
-        int n = s.length();
-        int ans = 0;
-        int count_b = 0;
-        // dp(i) gives the number of characters to be deleted to make substring
-        // s(0,i) valid corresponingly ans variable represents dp[i]
-        for (int i = 0; i < n; i++) {
-            // it is always valid to append b at the
-            if (s[i] == 'b')
-                count_b++;
-            else {
-                // two cases arise
-                // case1: we remove a
-                // case2: keep a, then count_b number of characters will need to
-                // be removed
-                ans = min(ans + 1, count_b);
-            }
+        int n = s.size(), res = n;
+        int a = 0, b = 0;
+
+        for (auto& c : s)
+            a += c & 1;             //'a' = 97 (odd)
+
+        for (auto& c : s) {
+            a -= c & 1;
+            res = min(res, a + b);
+            b += ~c & 1;           //'b' = 98 (even)
         }
-        return ans;
+
+        return res;
     }
 };
