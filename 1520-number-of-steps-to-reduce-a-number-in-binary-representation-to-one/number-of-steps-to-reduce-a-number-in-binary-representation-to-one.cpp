653c6ea533d68659fb060fb52bbd8033ef35c3b6
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2026-02-27 00:34:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (58.55%) - LeetSync

--- 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
+++ 1520-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/number-of-steps-to-reduce-a-number-in-binary-representation-to-one.cpp
@@ -1,18 +1,15 @@
-const static auto initialize = [] {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
 class Solution {
 public:
-    int numSteps(string& s) {
-        int ones = 0, last_one = 0;
-        for (auto i = 1; i < s.size(); ++i)
-            if (s[i] == '1') {
-                last_one = i;
-                ++ones;
-            }
-        return s.size() - 1 + (last_one - ones) + (last_one ? 2 : 0);
+    int numSteps(string s) {
+        int steps = 0, carry = 0;
+        for (int i = s.length() - 1; i > 0; i--) {
+            if ((s[i] == '1') + carry == 1) {
+                steps += 2;
+                carry = 1;
+            } else
+                steps += 1;
+        }
+        
+        return steps + carry;
     }
 };
