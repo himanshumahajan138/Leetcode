e048eef2cf358fbb6572e54d00e29743ed390e5f
Himanshu Mahajan <83700343+himanshumahajan138@users.noreply.github.com>
2026-03-09 00:17:22 +0530

    Time: 0 ms (100.00%) | Memory: 13.5 MB (23.51%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/find-unique-binary-string.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
-    string findDifferentBinaryString(vector<string>& nums) {
-        string result;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i][i] == '0') {
-                result += '1';
-            } else {
-                result += '0';
-            }
+    int len;
+    int toInt(string& nums){
+        int x=0;
+        for(char c: nums){
+            x=(x<<1)+(c-'0');
         }
-        return result;
+        return x;
+    }
+    string findDifferentBinaryString(vector<string>& nums) {
+        len=nums[0].size();
+        vector<bool> hasX(1<<len, 0);
+
+        for(auto& x:  nums)
+            hasX[toInt(x)]=1;
+        int N=0;//find N
+        for(; N<(1<<len) && hasX[N]; N++);
+    //    cout<<N<<endl;
+        string ans=string(len, '0');
+        for(int i=len-1; i>=0 && N>0; i--){
+            ans[i]=(N&1)+'0';
+            N>>=1;
+        }
+        return ans;
     }
 };
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 'c';
+}();
